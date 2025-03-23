1ed8fb62aead5560ad79de22a4b347f2568024b1
Trevor Pratt <pratttrevor79@gmail.com>
2025-03-23 15:12:40 -0500

    finally adding project to github, lots of commitments to push

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState } from "react";
+import all_product from "../Components/Assets/Frontend_Assets/all_product.js"
+import { useSearchParams } from "react-router-dom";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+    let cart = {};
+
+    for (let index = 0; index < all_product.length; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+ }
+
+const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems,] = useState(getDefaultCart()); 
+    
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]+1}))
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
